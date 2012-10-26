99eb98853e3c1bf2cbfab0d4f27e58156d1f062c
Frank Terbeck <ft@bewatermyfriend.org>
2012-10-26 12:53:33 +0200

    Add Test::TUI::VT102
    This module adds auxiliary functionality centered around the terminal
    implementation Test::TUI uses (namely `vt102').
    
    Currently implements `key()' a function to lookup escape sequences for
    special keys like Left-Arrow, Right-Arrow, Home, End or the function
    keys.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

---
+++ perl/Test/TUI/VT102.pm
@@ -0,0 +1,67 @@
+#!/usr/bin/perl
+
+package Test::TUI::VT102;
+
+use strict;
+use warnings;
+use diagnostics;
+use English '-no_match_vars';
+
+use Term::Cap;
+
+use Exporter;
+use base qw{ Exporter };
+use vars qw{ @EXPORT_OK };
+@EXPORT_OK = qw{ key };
+
+my (%keys,
+    $termcap);
+
+$termcap = Term::Cap->Tgetent({ TERM => "vt102",
+                                OSPEED => 9600});
+
+%keys = (
+    F1 => 'k1',
+    F2 => 'k2',
+    F3 => 'k3',
+    F4 => 'k4',
+    F5 => 'k5',
+    F6 => 'k6',
+    F7 => 'k7',
+    F8 => 'k8',
+    F9 => 'k9',
+    F10 => 'k;',
+    F11 => 'F1',
+    F12 => 'F2',
+    F13 => 'F3',
+    F14 => 'F4',
+    F15 => 'F5',
+    F16 => 'F6',
+    F17 => 'F7',
+    F18 => 'F8',
+    F19 => 'F9',
+    F20 => 'FA',
+    F21 => 'FB',
+    F22 => 'FC',
+    F23 => 'FD',
+    F24 => 'FE',
+    Home => 'kh',
+    End => '@7',
+    Insert => 'kI',
+    Backspace => 'bs',
+    Delete => 'dc',
+    Up => 'ku',
+    Down => 'kd',
+    Left => 'kl',
+    Right => 'kr',
+    PageUp => 'kP',
+    PageDown => 'kN'
+);
+
+sub key {
+    my ($k) = @_;
+    my $tc = $keys{$k};
+    return (defined $tc) ? $termcap->{"_$tc"} : q{};
+}
+
+1;
